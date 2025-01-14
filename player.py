c34981dec02b1c22a2a9c3c68c88fdce06f5ab4b
Dakota Riley <dakotacustompcs@gmail.com>
2025-01-14 18:49:02 -0500

    add rotate and update functions to the Player class in player.py

--- player.py
+++ player.py
@@ -20,3 +20,14 @@ class Player(CircleShape):
         color = ("white")
         line_width = 2
         pygame.draw.polygon(screen, color, self.triangle(), line_width)
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
